16e4daf3119b7c411cb08e7ca8c9197c5910de06
chenhongbo <chenhongbo@topscomm.com>
2020-08-25 09:17:55 +0800

    program mehr

--- hallo.cpp
+++ hallo.cpp
@@ -3,5 +3,16 @@ using namespace std;
 int main()
 {
 	cout<<"Hallo!"<<endl;
+	int wuhu=0;
+	cout<<"Ein Num giben Sie bitte!"<<endl;
+	cin>>wuhu;
+	if(wuhu>=10||wuhu<0)
+	{
+		cout<<"Du bist Scheisse!"<<endl;
+	}
+	else
+	{
+		cout<<"Fantastisch!"<<endl;
+	}
 	return 0;
 }
